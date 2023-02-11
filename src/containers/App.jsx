66a6fcecf8a952688ca3968bbc6b07353764b3d3
Rita Bradley <rita@ritabradley.dev>
2023-02-11 15:13:48 -0800

    setup animals state
    - return an array of animals
    - create random animal generator
    -when button is clicked, an image of a random animal is added

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react';
 import AnimalDisplay from '../components/AnimalDisplay';
 
+const getRandomAnimal = () => {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+};
+
 const App = () => {
+    let [animals, setAnimals] = useState([]);
+
+    const handleButtonClick = () => {
+        setAnimals([...animals, getRandomAnimal()]);
+    };
+
+    const displayedAnimals = animals.map((animal, index) => <AnimalDisplay key={index} type={animal} />);
+
     return (
-        <div>
-            <AnimalDisplay />
+        <div className='prose'>
+            <button className='btn btn-primary btn-wide text-primary-content' onClick={handleButtonClick}>
+                Add Animal
+            </button>
+            <div>{displayedAnimals}</div>
         </div>
     );
 };
