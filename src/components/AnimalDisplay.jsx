456887fe212e6d1089d0bd54e8e16533ed4316ed
Rita Bradley <rita@ritabradley.dev>
2023-02-11 16:05:19 -0800

    create the like button functionality
    - setup state with inital value at 0

--- src/components/AnimalDisplay.jsx
+++ src/components/AnimalDisplay.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+import heart from '../imgs/heart.svg';
 const AnimalDisplay = ({ type }) => {
+    const [likes, setLikes] = useState(0);
+    const handleClick = () => {
+        setLikes(likes + 1);
+    };
     return (
-        <div>
-            <img src={`../imgs/${type}.svg`} alt={type} />
+        <div onClick={handleClick}>
+            <img src={`../src/imgs/${type}.svg`} alt={type} />
+            <img src={heart} alt='heart icon' style={{ width: 10 + 10 * likes + 'px' }} />
         </div>
     );
 };
